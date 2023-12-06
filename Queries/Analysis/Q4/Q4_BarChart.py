3c565187e28570ff523a67521fd58ad43b0cd745
Martina99188 <martina_nolletti@yahoo.it>
2023-12-06 11:11:01 +0100

    q4&q5

--- Queries/Analysis/Q4/Q4_BarChart.py
+++ Queries/Analysis/Q4/Q4_BarChart.py
@@ -9,13 +9,13 @@ dfCsv = pd.DataFrame(csvFile)
 
 modifiedFilesDf = dfCsv["NumOfModifiedFiles"].dropna().to_numpy()
 
-barChartData = pd.cut(modifiedFilesDf, [0,1,3,6,10,np.inf], labels=['0','1-2','3-5','6-9','10+'])
+barChartData = pd.cut(modifiedFilesDf, [0,3,6,10,np.inf], labels=['0-2','3-5','6-9','10+'])
 barChart = barChartData.value_counts().plot.bar(color="green")
 
 plt.xticks(rotation=0)
 plt.title("Distribution of Modified Files by Commit")
 plt.xlabel('Number Of Modified Files')
-plt.ylabel('Number Of Commit') 
+plt.ylabel('Number Of Commit')
 
 barChart.bar_label(barChart.containers[0])
 
--- Queries/Analysis/Q5/Q5_BarChart.py
+++ Queries/Analysis/Q5/Q5_BarChart.py
@@ -9,11 +9,11 @@ dfCsv = pd.DataFrame(csvFile)
 
 modifiedFilesDf = dfCsv["CommitFrequency (days)"].dropna().to_numpy()
 
-barChartData = pd.cut(modifiedFilesDf, [0,1,2,5,9,np.inf], labels=['[0]','[1,2]','[3,5]','[6,9]','[10+]'])
+barChartData = pd.cut(modifiedFilesDf, [0,2,5,9,np.inf], labels=['[0,2]','[3,5]','[6,9]','[10+]'])
 barChart = barChartData.value_counts().plot.bar(color="green")
 
 plt.xticks(rotation=0)
-plt.title("Average Frequency Of Commitments For Repository")
+#plt.title("Average Frequency Of Commitments For Repository")
 plt.xlabel('Commit Frequency (Days)') 
 plt.ylabel('Number Of Repositories')
 
